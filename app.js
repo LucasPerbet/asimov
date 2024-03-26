b97bdd5fcc1ff1f8434ad750840b031857f3d59a
Ismael <ismael.aitlhoussaine@gmail.com>
2024-03-26 17:08:06 +0100

    Added views folders

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path'); // Import the path module
 
 const app = express();
 
 // configuration de EJS comme moteur de template
 app.set('view engine', 'ejs');
 // configuration du répertoire des vues
-app.set('views', __dirname + '/views');
+//app.set('views', __dirname + '/views');
+
+//app.set('views', path.join(__dirname, 'views'));
+
+app.set('views', [path.join(__dirname, '/views'),path.join(__dirname, 
+    '/views/eleves/'), path.join(__dirname, '/views/proviseur/'), path.join(__dirname, '/views/secretariat/'), path.join(__dirname, '/views/enseignant/'), path.join(__dirname, '/views/default/')]);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
