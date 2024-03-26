34a1b5044ddd5e063e05b5cad3a1fbc709d72d02
Lucas Perbet <lucas.perbet1997@hotmail.com>
2024-03-26 17:18:25 +0100

    style + navabar

--- model/loginModel.js
+++ model/loginModel.js
@@ -4,9 +4,8 @@ async function getUtilisateurByLogin(id_utilisateur, mdp) {
     try {
 
         // Déclaration de la requête
-     //   const sql = "SELECT * FROM utilisateur WHERE id_utilisateur = ? AND mdp = ?";
-    const sql = "SELECT *, libelle_role FROM utilisateur JOIN role ON utilisateur.id_role = role.id_role WHERE id_utilisateur = ? AND mdp = ?;"
-
+        //   const sql = "SELECT * FROM utilisateur WHERE id_utilisateur = ? AND mdp = ?";
+        const sql = "SELECT *, libelle_role FROM utilisateur JOIN role ON utilisateur.id_role = role.id_role WHERE id_utilisateur = ? AND mdp = ?;"
 
         // Exécution de la requête, et récupération du résultat dans rows
         const [rows, fields] = await pool.query(sql, [id_utilisateur, mdp]);
